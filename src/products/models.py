2b62d6c18eaaec55422fd0e118806c6f6a975b75
EthanK28 <kes0228@gmail.com>
2016-02-13 02:56:49 +0900

    static, media, protected roots added in settings.py

--- src/products/models.py
+++ src/products/models.py
@@ -1,4 +1,5 @@
 from django.conf import settings
+from django.core.files.storage import FileSystemStorage
 from django.db import models
 from django.db.models.signals import pre_save, post_save
 from django.core.urlresolvers import reverse
@@ -8,14 +9,23 @@ from django.utils.text import slugify
 # Create your models here.
 
 
+
+def download_mdedia_location(instance, filename):
+    return "%s/%s" %(instance.id, filename)
+
 class Product(models.Model):
     user = models.ForeignKey(settings.AUTH_USER_MODEL)
     managers = models.ManyToManyField(settings.AUTH_USER_MODEL, related_name="managers_product", blank=True)
     title = models.CharField(max_length=140)  # sdlfldsfjaslk
+    media = models.FileField(blank=True,
+                             null=True,
+                             upload_to=download_mdedia_location,
+                             storage=FileSystemStorage(location=settings.PROTECTED_ROOT))
     slug = models.SlugField(blank=True, unique=True)#unique=True)
     description = models.TextField()
     price = models.DecimalField(max_digits=100, decimal_places=2, default=9.99)
     sale_price = models.DecimalField(max_digits=100, decimal_places=2, default=6.99, blank=True, null=True)
+
 
 
 
@@ -26,6 +36,24 @@ class Product(models.Model):
         viewname = "product_detail_slug_view"
         return reverse(viewname, kwargs={"slug": self.slug})
 
+    def get_download(self):
+        view_name = "products:download_slug"
+
+class Thumbnail(models.Model):
+    product = models.ForeignKey(Product)
+    user = models.ForeignKey(settings.AUTH_USER_MODEL)
+    height = models.CharField(max_length=20, null=True, blank=True)
+    width = models.CharField(max_length=20, null=True, blank=True)
+    media = models.ImageField(
+                            width_field= "width",
+                            height_field= "height",
+                            blank=True,
+                            null=True,
+                            uplod_to=download_mdedia_location
+    )
+
+    def __str__(self):
+        return str(self.media.path)
 
 
 def create_slug(instance, new_slug=None):
--- src/products/views.py
+++ src/products/views.py
@@ -1,18 +1,25 @@
-from django.http import Http404
-from django.shortcuts import render, get_object_or_404
+import os
 
+from mimetypes import guess_type
+
+from django.conf import settings
+from django.http import Http404, HttpResponse
+from django.shortcuts import render, get_object_or_404
 from django.views.generic.edit import CreateView, UpdateView
 from django.views.generic.detail import DetailView
 from django.views.generic.list import ListView
+from django.core.servers.basehttp import FileWrapper
+from django.db.models import Q
+
 
 # Create your views here.
 
 from digitalmarket.mixins import (
-    MultiSlugMixin,
-    SubmitBtnMixin,
-    LoginRequiredMixin,
-    StaffRequiredMixin
+	MultiSlugMixin,
+	SubmitBtnMixin,
+	LoginRequiredMixin
 )
+
 
 from .forms import ProductAddForm, ProductModelForm
 from .mixins import ProductManagerMixin
@@ -24,80 +31,118 @@ from .models import Product
 
 
 
+class ProductDownloadView(MultiSlugMixin, DetailView):
+	model = Product
 
-class ProductCreateView(SubmitBtnMixin, CreateView):
-    model = Product
-    template_name = "form.html"
-    form_class = ProductModelForm
-    # success_url = "/products/"
-    submit_btn = "Add Product"
+	def get(self, request, *args, **kwargs):
+		obj = self.get_object()
+		if obj in request.user.myproducts.products.all():
+			filepath = os.path.join(settings.PROTECTED_ROOT, obj.media.path)
+			guessed_type = guess_type(filepath)[0]
 
-    def form_valid(self, form):
-        user = self.request.user
-        form.instance.user = user
-        valid_data = super(ProductCreateView, self).form_valid(form)
-        form.instance.managers.add(user) # default 매니저 넣는곳
-        # add all equal users
-        return valid_data
+			f = open(filepath, 'rb')
+			wrapper = FileWrapper(f)
+
+			mimetype = 'application/force-download'
+			if guessed_type:
+				mimetype = guessed_type
+			response = HttpResponse(wrapper, content_type=mimetype)
+
+			if not request.GET.get("preview"):
+				response["Content-Disposition"] = "attachment; filename=%s" %(obj.media.name)
+
+			response["X-SendFile"] = str(obj.media.name)
+			return response
+		else:
+			raise Http404
 
 
-    # def get_success_url(self):
-    #     return "/users/%s/" %(self.request.user)
-        # return "/products/"
+
+
+class ProductCreateView(LoginRequiredMixin, SubmitBtnMixin, CreateView):
+	model = Product
+	template_name = "form.html"
+	form_class = ProductModelForm
+	#success_url = "/products/"
+	submit_btn = "Add Product"
+
+	def form_valid(self, form):
+		user = self.request.user
+		form.instance.user = user
+		valid_data = super(ProductCreateView, self).form_valid(form)
+		form.instance.managers.add(user)
+		# add all default users
+		return valid_data
+
+	# def get_success_url(self):
+	# 	return reverse("products:list")
 
 
 
 
 class ProductUpdateView(ProductManagerMixin, SubmitBtnMixin, MultiSlugMixin, UpdateView):
-    model = Product
-    template_name = "form.html"
-    form_class = ProductModelForm
-    success_url = "/products/"
-    submit_btn = "Update Product"
 
-    # def get_object(self, *args, **kwargs):
-    #     user = self.request.user
-    #     print(user)
-    #     obj = super(ProductUpdateView, self).get_object(*args, **kwargs)
-    #     print("obj", obj)
-    #     if obj.user == user or user in obj.managers.all():
-    #         return obj
-    #     else:
-    #         raise Http404
+	model = Product
+	template_name = "form.html"
+	form_class = ProductModelForm
+	success_url = "/products/"
+	submit_btn = "Update Product"
+
+	# def get_object(self, *args, **kwargs):
+	#     user = self.request.user
+	#     print(user)
+	#     obj = super(ProductUpdateView, self).get_object(*args, **kwargs)
+	#     print("obj", obj)
+	#     if obj.user == user or user in obj.managers.all():
+	#         return obj
+	#     else:
+	#         raise Http404
 
 
 
 class ProductDetailView(MultiSlugMixin, DetailView):
-    model = Product
+	model = Product
 
-    # def get_object(self, *args, **kwargs):
-    #     slug = self.kwargs.get("slug")
-    #     ModelClass = self.model
-    #     print (slug)
-    #     if slug is not None:
-    #         try:
-    #             obj = get_object_or_404(ModelClass, slug=slug)
-    #         except ModelClass.MultipleObjectsReturned:
-    #             obj = ModelClass.objects.filter(slug=slug).order_by("-title").first()
-    #     else:
-    #         obj = super(ProductDetailView, self).get_context_data(**kwargs)
-    #
-    #     return obj
+	# def get_object(self, *args, **kwargs):
+	#     slug = self.kwargs.get("slug")
+	#     ModelClass = self.model
+	#     print (slug)
+	#     if slug is not None:
+	#         try:
+	#             obj = get_object_or_404(ModelClass, slug=slug)
+	#         except ModelClass.MultipleObjectsReturned:
+	#             obj = ModelClass.objects.filter(slug=slug).order_by("-title").first()
+	#     else:
+	#         obj = super(ProductDetailView, self).get_context_data(**kwargs)
+	#
+	#     return obj
 
 class ProductListView(ListView):
-    model = Product
 
-    # template_name = "list_view.html"
-    # def get_context_data(self, **kwargs):
-    #     context = super(ProductListView, self).get_context_data(**kwargs)
-    #     print(context)
-    #     context["queryset"] = self.get_queryset()
-    #     return context
 
-    def get_queryset(self, *args, **kwargs):
-        qs =  super(ProductListView, self).get_queryset(**kwargs)
-        # qs =  qs.filter(title__icontains="aaaaaa")
-        return qs
+	model = Product
+
+	def get_queryset(self, *args, **kwargs):
+		qs = super(ProductListView, self).get_queryset(**kwargs)
+		query = self.request.GET.get("q")
+		if query:
+			qs = qs.filter(
+					Q(title__icontains=query)|
+					Q(description__icontains=query)
+				).order_by("title")
+		return qs
+
+	# template_name = "list_view.html"
+	# def get_context_data(self, **kwargs):
+	#     context = super(ProductListView, self).get_context_data(**kwargs)
+	#     print(context)
+	#     context["queryset"] = self.get_queryset()
+	#     return context
+
+	def get_queryset(self, *args, **kwargs):
+		qs =  super(ProductListView, self).get_queryset(**kwargs)
+		# qs =  qs.filter(title__icontains="aaaaaa")
+		return qs
 
 
 
@@ -105,92 +150,92 @@ class ProductListView(ListView):
 
 
 def create_view(request):
-    # Form
-    form = ProductModelForm(request.POST or None)
-    
+	# Form
+	form = ProductModelForm(request.POST or None)
 
-    if form.is_valid():
-        print(form.cleaned_data.get("publish"))
-        instance = form.save(commit=False)
-        instance.sale_price = instance.price
-        form.save()
 
-    template = "form.html"
-    context = {
-        "form": form,
-        "submit_btn": "Save Product"
-    }
-    return render(request, template, context)
+	if form.is_valid():
+		print(form.cleaned_data.get("publish"))
+		instance = form.save(commit=False)
+		instance.sale_price = instance.price
+		form.save()
+
+	template = "form.html"
+	context = {
+		"form": form,
+		"submit_btn": "Save Product"
+	}
+	return render(request, template, context)
 
 def update_view(request, object_id=None):
-    product = get_object_or_404(Product, id=object_id)
-    form = ProductModelForm(request.POST or None, instance=product)
-    if form.is_valid():
-        instance = form.save(commit=False)
-        form.save()
+	product = get_object_or_404(Product, id=object_id)
+	form = ProductModelForm(request.POST or None, instance=product)
+	if form.is_valid():
+		instance = form.save(commit=False)
+		form.save()
 
 
-    template = "form.html"
-    context = {
-        # "object": product,
-        "form": form,
-        "submit_btn": "Update Product"
-    }
-    return render(request, template, context)
+	template = "form.html"
+	context = {
+		# "object": product,
+		"form": form,
+		"submit_btn": "Update Product"
+	}
+	return render(request, template, context)
 
 
 def detail_slug_view(request, slug=None):
-    try:
-        product = get_object_or_404(Product, slug=slug)
-    except Product.MultipleObjectsReturned:
-        product = Product.objects.filter(slug=slug).order_by("-title").first()
+	try:
+		product = get_object_or_404(Product, slug=slug)
+	except Product.MultipleObjectsReturned:
+		product = Product.objects.filter(slug=slug).order_by("-title").first()
 
-    # print(slug)
-    # product = 1
-    template = "detail_view.html"
-    context = {
-        "object": product
-    }
-    return render(request, template, context)
+	# print(slug)
+	# product = 1
+	template = "detail_view.html"
+	context = {
+		"object": product
+	}
+	return render(request, template, context)
 
 
 
 def detail_view(request, object_id=None):
-    product = get_object_or_404(Product, id=object_id)
+	product = get_object_or_404(Product, id=object_id)
 
-    template = "detail_view.html"
-    context = {
-        "object": product
-    }
-    return render(request, template, context)
-    # if object_id is not None:
+	template = "detail_view.html"
+	context = {
+		"object": product
+	}
+	return render(request, template, context)
+	# if object_id is not None:
 
-        # try:
-        #     product = Product.objects.get(id=object_id)
-        # except Product.DoesNotExist:
-        #     product = None
-        # template = "detail_view.html"
-        # context = {
-        #     "object": product
-        # }
-        # return render(request, template, context)
-    # else:
-    #     raise Http404
+		# try:
+		#     product = Product.objects.get(id=object_id)
+		# except Product.DoesNotExist:
+		#     product = None
+		# template = "detail_view.html"
+		# context = {
+		#     "object": product
+		# }
+		# return render(request, template, context)
+	# else:
+	#     raise Http404
 
 
 
 
 
 def list_view(request):
-    # list of items
-    print(request)
+	# list of items
+	print(request)
 
-    queryset = Product.objects.all()
-    template = "list_view.html"
-    context = {
-        "queryset": queryset
-    }
-    return render(request, template, context)
+	queryset = Product.objects.all()
+	template = "list_view.html"
+	context = {
+		"queryset": queryset
+	}
+	return render(request, template, context)
 
 
 
